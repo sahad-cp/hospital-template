8a3544406ea9a743d683add398de940931048ccd
sahad-cp <sahadmanu4@gmail.com>
2023-01-05 18:06:56 +0530

    'dashbord'

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,6 +5,9 @@ from dashboard import views
 urlpatterns = [
     
     path('dashboard/home',views.Home,name='dash_home'),
+    path('dashboard/booking',views.Home,name='dash_booking'),
+    path('dashboard/department',views.Home,name='dash_department'),
+    path('dashboard/doctor',views.Home,name='dash_doctor'),
     
     
 ]
